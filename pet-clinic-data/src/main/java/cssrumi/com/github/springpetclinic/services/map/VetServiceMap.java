c4fc2838cefde571e2107d95e75421b9251f392b
Adam Rumiński <Adam.Ruminski@atena.pl>
2019-02-18 13:24:15 +0100

    Implemented Map based services, closes #6

---
+++ pet-clinic-data/src/main/java/cssrumi/com/github/springpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package cssrumi.com.github.springpetclinic.services.map;
+
+import cssrumi.com.github.springpetclinic.model.Vet;
+import cssrumi.com.github.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return findById(id);
+    }
+}
